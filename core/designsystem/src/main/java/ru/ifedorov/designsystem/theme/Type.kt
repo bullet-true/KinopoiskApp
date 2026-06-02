f420abc7a63c44f93ffdb1ab4655ead154f5a6fa
Igor Fedorov <bullet-1@yandex.ru>
2026-06-02 17:26:31 +0500

    KA-6: add font

--- core/designsystem/src/main/java/ru/ifedorov/designsystem/theme/Type.kt
+++ core/designsystem/src/main/java/ru/ifedorov/designsystem/theme/Type.kt
@@ -2,42 +2,50 @@ package ru.ifedorov.designsystem.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import ru.ifedorov.designsystem.R
 
-// Set of Material typography styles to start with
+private val GraphikFontFamily = FontFamily(
+    Font(
+        resId = R.font.graphik_regular,
+        weight = FontWeight.Normal
+    )
+)
+
 val Typography = Typography(
     headlineMedium = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = GraphikFontFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 24.sp,
         lineHeight = 32.sp,
         letterSpacing = 0.sp
     ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = GraphikFontFamily,
         fontWeight = FontWeight.SemiBold,
         fontSize = 20.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = GraphikFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.sp
     ),
     bodyMedium = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = GraphikFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.sp
     ),
     labelLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = GraphikFontFamily,
         fontWeight = FontWeight.SemiBold,
         fontSize = 14.sp,
         lineHeight = 16.sp,
