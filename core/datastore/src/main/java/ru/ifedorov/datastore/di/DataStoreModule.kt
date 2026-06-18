5123369afd4ea6a086560d8e815c0fec6a316ef7
Igor Fedorov <bullet-1@yandex.ru>
2026-06-18 15:43:51 +0500

    KA-8: add datastore module class

---
+++ core/datastore/src/main/java/ru/ifedorov/datastore/di/DataStoreModule.kt
@@ -0,0 +1,27 @@
+package ru.ifedorov.datastore.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+private const val APP_PREFERENCES_NAME = "app_preferences"
+
+private val Context.appPreferencesDataStore by preferencesDataStore(name = APP_PREFERENCES_NAME)
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+
+    @Provides
+    @Singleton
+    fun provideAppPreferencesDataStore(
+        @ApplicationContext context: Context
+    ): DataStore<Preferences> = context.appPreferencesDataStore
+}
