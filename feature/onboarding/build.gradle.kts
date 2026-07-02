c72dc5c9047f06a804a2b1c04b08f6f1ceaa2edc
Igor Fedorov <bullet-1@yandex.ru>
2026-07-02 20:49:24 +0500

    KA-11: add onboarding logo

--- feature/onboarding/build.gradle.kts
+++ feature/onboarding/build.gradle.kts
@@ -31,5 +31,6 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.foundation)
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
